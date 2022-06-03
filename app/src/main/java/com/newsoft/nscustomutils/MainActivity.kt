17020df8a7942528e3307934dedb360486ca2440
Viet Anh <dinhanh138@gmail.com>
2022-06-03 10:24:55 +0700

    add slide

--- app/src/main/java/com/newsoft/nscustomutils/MainActivity.kt
+++ app/src/main/java/com/newsoft/nscustomutils/MainActivity.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 
 class MainActivity : BaseActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         switchFragment(Fragment())
     }
-
-
 }
